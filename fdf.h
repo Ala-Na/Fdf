1d5b404362a7295098a0506efa0e51f4ad344b38
anadege <ilmefautuneadresse@gmail.com>
2021-07-05 12:05:56 +0200

    Progress

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 14:29:19 by anadege           #+#    #+#             */
-/*   Updated: 2021/07/02 17:49:58 by anadege          ###   ########.fr       */
+/*   Updated: 2021/07/05 11:44:49 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@
 # define WIN_LENGTH 1000
 # define WIN_HEIGHT 800
 # define ALPHA 30
+# define PI 3.14159265359
 
 /*
 ** Structure for stocking map content
@@ -47,14 +48,18 @@ typedef struct s_param
 	int		valid;
 	void	*mlx;
 	void	*window;
-	int		win_length;
-	int		win_height;
-	int		i_center; // = win_length / 2;
-	int		j_center; // = win_height / 2;
+	void	*img;
+	int		win_length; //WIN_LENGTH
+	int		win_height; // WIN_HEIGHT
+	int		img_length; // win_length - cste pour éviter bords
+	int		img_height; // m chose
+	int		i_center; // = img_length / 2;
+	int		j_center; // = img_height / 2;
 	int		i_max;
 	int		j_max;
 	int		alpha;
 	int		pix_per_seg;
+	int		default_color;
 	t_map	*map_infos;
 }	t_param;
 
--- isometric_rep.c
+++ isometric_rep.c
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 22:36:32 by anadege           #+#    #+#             */
-/*   Updated: 2021/07/02 17:52:10 by anadege          ###   ########.fr       */
+/*   Updated: 2021/07/05 12:05:38 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,49 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
-#define PI 3.14159265359
+#include "fdf.h"
 
 /*
 ** Function to convert any 3D x/y/z coordinates into 2D coordinates i/j with an
 ** isometric representation according to an alpha angle along the x axis. Alpha is set to 30 
 ** degree as default but can be modified to have a rotation effect.
 */
-void	twoD_iso_coordinates(int x, int y, int z)
+void	twod_iso_coordinates(t_param *param, t_point *point)
 {
 	double	i;
 	double	j;
-	double alpha = 30;
-	double	val = PI / 180;
+	double	alpha;
+	double	radian;
 
-	printf("cos %f is %f radian\n", alpha, alpha * val);
-	i = x * cos(alpha * val) + y * cos((alpha + 120) * val) + z * cos((alpha - 120) * val);
-	j = x * sin(alpha * val) + y * sin((alpha + 120) * val) + z * sin((alpha - 120) * val);
-	printf("x %i y %i z %i become i %f j %f\n", x, y, z, i, j);
-	//go to draw ? but before pix_per_seg to calculate ? and problem with need of multipe coordinate to draw seg
+	alpha = ALPHA;
+	radian = PI / 180;
+	// Obtain coordinates by calculus in a 2D environment
+	i = point->x * cos(alpha * radian) + point->y * cos((alpha + 120) * radian) + point->z * cos((alpha - 120) * radian);
+	j = point->x * sin(alpha * radian) + point->y * sin((alpha + 120) * radian) + point->z * sin((alpha - 120) * radian);
+	// Replace point with center of the image as center
+	i += (double)param->i_center;
+	j += (double)param->j_center;
+	// Stock result
+	point->i = (int)i;
+	point->j = (int)j;
 }
 
-void	recup_threeD_coordinates(t_param *param)
+void	recup_threed_z(t_param *param, t_point *point)
 {
-	int	x;
-	int	y;
-	int	z = 0;
+	int	z;
+
+	z = param->map_infos->lines_content[point->x + point->y * param->map_infos->lines_size];
+	point->z = z;
+
+}
+
+void	recup_threed_coordinates(t_param *param)
+{
+	int		x;
+	int		y;
+	int		z;
+	t_point	*prev_point;
+	t_point	*curr_point;
 
 	// Vertical
 	x = 0;
@@ -49,7 +65,6 @@ void	recup_threeD_coordinates(t_param *param)
 		y = 0;
 		while (y < param->map_infos->nbr_total_lines)
 		{
-			z = param->map_infos->lines_content[x + y * param->map_infos->lines_size];
 			// get iso coordinate and draw segment ?
 			y += 1;
 		}
@@ -69,15 +84,4 @@ void	recup_threeD_coordinates(t_param *param)
 		}
 		y += 1;
 	}
-}
-
-int main(int argc, char **argv)
-{
-	if (argc != 4)
-		return (1);
-	int x = atoi(argv[1]);
-	int y = atoi(argv[2]);
-	int z = atoi(argv[3]);
-	twoD_iso_coordinates(x, y, z);
-	return (0);
 }
